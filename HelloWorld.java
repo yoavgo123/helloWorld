8cd5f6fe0ff21732fddf38c389ac4badf26d8c51
yoavgo123 <yoavgo123@gmail.com>
2016-04-27 01:46:52 +0300

    cccccccccccccc

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@
 public class HelloWorld {
 	
 	public static void main(String[] args){
-		System.out.print("HelloWorld *************pppppppppppppppp***************");
+		System.out.print("HelloWorld *************rrrrrrrrrrrrrrrrrr***************");
 	}
 
 }
